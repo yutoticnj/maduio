538bdfcfde402e7b09d363aad955748d7e7ee928
yutoticnj <gulianfugua1030@yeah.net>
2021-07-26 10:01:04 +0800

    202203022

---
+++ day11/src/com/akie/java/ExtendsTest.java
@@ -0,0 +1,15 @@
+package com.akie.java;
+
+public class ExtendsTest {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.age = 18;
+        p1.eat();
+
+        Student s1 = new Student();
+        s1.name = "Tom";
+        s1.sleep();
+        s1.study();
+    }
+
+}
