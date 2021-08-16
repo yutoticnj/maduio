403264b3499f32db1fb14751aa1078f8e50236da
yutoticnj <gulianfugua1030@yeah.net>
2021-08-16 17:21:44 +0800

    20220403

--- day13/src/com/akie/exer4/ScoreTest.java
+++ day13/src/com/akie/exer4/ScoreTest.java
@@ -6,15 +6,15 @@ import java.util.Vector;
 /*
 * 利用Vector代替数组处理：从键盘读入学生成绩（以负数代表输入结束），找出最高分，并输出学生成绩等级。
 
-提示：数组一旦创建，长度就固定不变，所以在创建数组前就需要知道它的长度。
+* 提示：数组一旦创建，长度就固定不变，所以在创建数组前就需要知道它的长度。
 * 而向量类 java.util.Vector 可以根据需要动态伸缩。
 *
-创建 Vector 对象： Vector v=new Vector();
-给向量添加元素 v.addElement(Object obj);   //obj 必须是对象
-取出向量中的元素： Object obj = v.elementAt(0)
-注意第一个元素的下标是0，返回值是Object类型的。
-计算向量的长度：v.size();
-若与最高分相差10分内：A等； 20分内：B等；30分内：C等；其它： D等
+* 创建 Vector 对象： Vector v=new Vector();
+* 给向量添加元素 v.addElement(Object obj);   //obj 必须是对象
+* 取出向量中的元素： Object obj = v.elementAt(0)
+* 注意第一个元素的下标是0，返回值是Object类型的。
+* 计算向量的长度：v.size();
+* 若与最高分相差10分内：A等； 20分内：B等；30分内：C等；其它： D等
 
 * */
 public class ScoreTest {
