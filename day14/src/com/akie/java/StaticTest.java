4dc03190bee274f7e616c390d2e754a90ae8e225
yutoticnj <gulianfugua1030@yeah.net>
2021-08-19 13:16:14 +0800

    20220411

--- day14/src/com/akie/java/StaticTest.java
+++ day14/src/com/akie/java/StaticTest.java
@@ -7,24 +7,63 @@ package com.akie.java;
 * 2、static可以用来修饰：属性、方法、代码块、内部类
 *
 *
-* 3、使用static修饰属性：
+* 3、使用static修饰属性：静态变量(或类变量)
+*       3.1 属性：按是否使用static修饰，又分为：静态属性  vs  非静态属性(实例变量)
 *
+*       实例变量：我们创建了类的多个对象，每个对象都独立拥有一套类中的非静态属性。
+*       当修改其中一个对象的费静态属性时，不会导致其他对象中属性值得修改。
 *
-* 4、使用static修饰方法：
+*       静态变量：我们创建了类的多个对象，每个对象都共享同一个静态变量。当通过某一个对象修改静态变量时，
+*       会导致其他对象调用此静态变量时，是修改过了的。
+*
+*       3.2 static修饰属性的其他说明：
+*           静态变量随着类的加载而加载。可以通过“类.静态变量”的方式进行调用
+*           静态变量的加载要早于对象的创建。
+*           由于类只加载一次，则静态变量在内存中也只会存在一份，存在方法区的静态域中。
+*
+*               类变量    实例变量
+*       类        yes       no
+*       对象      yes       yes
+*
+*       3.3静态属性举例： System.out; Math.PI;
+*
+* 4、使用static修饰方法：静态方法
+*       1 随着类的加载而加载，可以通过“类.静态方法”的方式调用
+*       2       静态方法    非静态方法
+ *       类        yes       no
+ *       对象      yes       yes
+ *      3 静态方法中，只能调用静态的方法或属性
+ *        非静态方法中，既可以调用非静态的方法或属性，也可以调用静态的方法或属性
+*
+* 5 static注意点
+*       在静态的方法内，不能使用this关键字、super关键字
+*
+* 6、开发中，如何确定一个属性声明为static？
+*       属性时可以被多个对象所共享的，不会随着对象的不同而不同。
+*
+*    开发中，如何确定一个方法声明为static？
+*       操作静态属性的方法，通常设置为static的
+*       工具类中的方法，习惯上声明为static的      比如：Math、Arrays、Collections
 * */
 public class StaticTest {
     public static void main(String[] args) {
 
+        Chinese.nation = "中国";
+
         Chinese c1 = new Chinese();
         c1.name = "张三";
         c1.age = 22;
+        c1.nation = "CHN";
 
         Chinese c2 = new Chinese();
         c2.name = "李四";
         c2.age = 25;
+        c2.nation = "China";
 
 
 
+        System.out.println(c2.nation);
+        Chinese.show();
 
     }
 }
@@ -33,5 +72,30 @@ class Chinese {
 
     String name;
     int age;
+    static String nation;
 
+    public void eat(){
+        System.out.println("中国人吃中餐");
+        //调用非静态结构
+        info();
+        System.out.println("name" + name);
+
+        //调用静态结构
+        walk();
+        System.out.println("nation" + nation);
+    }
+
+    public static void show() {
+        System.out.println("我是一个中国人！");
+        //只能调用静态的结构
+        System.out.println(nation);
+        walk();
+    }
+
+    public void info(){
+        System.out.println("name" + name + ",age" + age);
+    }
+
+    public static void walk(){}
 }
+
